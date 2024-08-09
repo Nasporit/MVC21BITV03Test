9b71734fa33b9193f37fbfd4a8888f70bddabf2e
Nasporit <160713625+Nasporit@users.noreply.github.com>
2024-08-09 15:44:48 +0700

    Add files via upload

---
+++ Controllers/Customers.cs
@@ -0,0 +1,37 @@
+﻿using EFCoreFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class CustomersController : Controller
+{
+    private readonly CarDealerContext _context;
+
+    public CustomersController(CarDealerContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Customer customer)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Customer.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(customer);
+    }
+
+    public IActionResult Index()
+    {
+        var customers = _context.Customer.ToList();
+        return View(customers);
+    }
+}
